22152ffb32ed3ec58c610e7113991ad08d42339e
Renat <60031371+NiXbi-L@users.noreply.github.com>
2023-09-06 14:38:16 +1000

    Update cfg.py
    ф

--- cfg.py
+++ cfg.py
@@ -1,6 +1,6 @@
-ShopID =  '988930' #'987790'
-PaymentAPI_key = 'test_8uqHtQ94DrghzUajQBeAPf-MTcBs7ICmDTfW-DCGFWU' #'live_6KfwBVPhSqibMV2Y3bmk45LO6fthCHf9IsPp_dlhiGg'
-databaseurl = 'http://192.168.1.143:1501/' #Url БД
-DataFrameAPI_Token = 'sR9ghISk1VRTlJMKewJYkWRAlhlsT4f00OYosO4Fpv62hsVeBlC5uWSoaiQ2qc7i' #Токен обращения к БД
-from_email = 'bosaix@mail.ru' #Email
-password = 'fBzTdBR9MufhwB0Zy772' #Email Pass
+ShopID =  '' #''
+PaymentAPI_key = '' #''
+databaseurl = '' #Url БД
+DataFrameAPI_Token = '' #Токен обращения к БД
+from_email = '' #Email
+password = '' #Email Pass
